b00c18610aad3ad46459d4bb8c84456bdf5ee3bd
Keith Yong <terda12@gmail.com>
2014-10-04 16:53:04 -0400

    Added stylesheet

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html>
     <h1>I'm Too Lazy To...</h1>
+    <head><link rel="stylesheet" type="text/css" href="stylesheet.css">
+    </head>
     <body>
         <?php print('hi'); ?>
     </body>
