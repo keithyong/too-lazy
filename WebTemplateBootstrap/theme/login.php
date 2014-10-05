4679904ebdb5eef0860971ba14281461f16fce3e
Karan <bNastyK@udel.edu>
2014-10-05 09:15:35 -0400

    eddit

--- WebTemplateBootstrap/theme/login.php
+++ WebTemplateBootstrap/theme/login.php
@@ -1,14 +1,10 @@
 
 
 <?php
+
 ini_set("display_errors",1);
 error_reporting(-1);
-$con=mysqli_connect(
-	"mydbinstance.czuwmj0nklpa.us-west-2.rds.amazonaws.com",
-	"keithy",
-	"hackathon",
-	"db64"
-	);
+$con=mysqli_connect("mydbinstance.czuwmj0nklpa.us-west-2.rds.amazonaws.com","keithy","hackathon","db64");
 // Check connection
 if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
@@ -22,13 +18,6 @@ if(preg_match('/[a-zA-Z0-9_]+@udel\.edu/',$_POST["email"])){
 
 	mysqli_query($con, "INSERT INTO Users(Email, myPassword)
 		VALUES ('$email', '$pass')");
-
-	$result = mysqli_query($con,"SELECT * FROM Users");
-
-	while($row = mysqli_fetch_array($result)) {
-	  echo $row['Email'] . " " . $row['myPassword'];
-	  echo "<br>";
-	}	
 
 	header('\forum.html');
 
