7ad8ce07d872ddf88221fec46554cfe99da7e5f8
Karan <bNastyK@udel.edu>
2014-10-05 05:18:50 -0400

    added login2

--- WebTemplateBootstrap/theme/login2.php
+++ WebTemplateBootstrap/theme/login2.php
@@ -1,35 +1,41 @@
 <?php
 
-	$con=mysqli_connect(
+	mysqli_connect(
 		"mydbinstance.czuwmj0nklpa.us-west-2.rds.amazonaws.com",
 		"keithy",
 	"	hackathon",
 		"innodb"
 		);
 	
-	function Login2()
-		{
-			if(empty($_POST["email"])) {
-				$this->HandleError("UserName is empty!");
-			return false;
-			}
-			if(empty($_POST['pass']))
-		{
-			$this->HandleError("Password is empty!");
-			return false;
-		}
-		
-		$username = trim($_POST["email"]);
-		$password = trim($_POST["password"]);
-     
-		if(!$this->CheckLoginInDB($username,$password))
-		{
-			return false;
-		}
-     
-		session_start();
-     
-		$_SESSION[$this->GetLoginSessionVar()] = $username;
-     
-		return true;
-	}
+	
+// checkLogin.php
+
+session_start(); // Start a new session
+require(login.php); // Holds all of our database connection information
+
+// Get the data passed from the form
+$username = $_POST['email'];
+$password = $_POST['pass'];
+
+// Do some basic sanitizing
+$username = stripslashes($username);
+$password = stripslashes($password);
+
+$sql = "select * from users where username = '$username' and password = '$password'";
+$result = mysql_query($sql) or die ( mysql_error() );
+
+$count = 0;
+
+while ($line = mysql_fetch_assoc($result)) {
+	 $count++;
+}
+
+if ($count == 1) {
+	 $_SESSION['loggedIn'] = "true";
+	 header("Location: loginSuccess.php"); // This is wherever you want to redirect the user to
+} else {
+	 $_SESSION['loggedIn'] = "false";
+	 header("Location: loginFailed.php"); // Wherever you want the user to go when they fail the login
+}
+
+?>
