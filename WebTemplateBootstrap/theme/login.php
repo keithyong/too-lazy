15573eb22ebcb9cbd98cc699295d26b72527368f
Keith Yong <terda12@gmail.com>
2014-10-05 04:51:21 -0400

    some changes

--- WebTemplateBootstrap/theme/login.php
+++ WebTemplateBootstrap/theme/login.php
@@ -2,6 +2,8 @@
 <body>
 
 <?php
+ini_set("display_errors",1);
+error_reporting(-1);
 $con=mysqli_connect(
 	"mydbinstance.czuwmj0nklpa.us-west-2.rds.amazonaws.com",
 	"keithy",
@@ -13,18 +15,21 @@ if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
-$email = $_POST["email"];
-$pass = $_POST["pass"];
-$passconfirm = $_POST["passconfirm"];
+if(preg_match('/[a-zA-Z0-9_]+@udel\.edu/',$_POST["email"])){
+	echo 'matched';
+	$email = $_POST["email"];
+	$pass = $_POST["pass"];
+	$passconfirm = $_POST["passconfirm"];
 
-mysqli_query($con, "INSERT INTO Users(Email, myPassword)
-	VALUES ('$email', '$pass')");
+	mysqli_query($con, "INSERT INTO Users(Email, myPassword)
+		VALUES ('$email', '$pass')");
 
-$result = mysqli_query($con,"SELECT * FROM Users");
+	$result = mysqli_query($con,"SELECT * FROM Users");
 
-while($row = mysqli_fetch_array($result)) {
-  echo $row['Email'] . " " . $row['myPassword'];
-  echo "<br>";
+	while($row = mysqli_fetch_array($result)) {
+	  echo $row['Email'] . " " . $row['myPassword'];
+	  echo "<br>";
+	}	
 }
 
 ?>
