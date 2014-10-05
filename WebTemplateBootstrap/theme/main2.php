1d484417045861ca600ee25b604870463ce29ad7
Karan <bNastyK@udel.edu>
2014-10-05 08:02:27 -0400

    bullshit

--- WebTemplateBootstrap/theme/main2.php
+++ WebTemplateBootstrap/theme/main2.php
@@ -1,9 +1,30 @@
+<html>
+<body>
 <?php
-include_once 'registerdata.php';
-$mysqli = new mysqli(HOST,USER,PASSWORD,DATABASE);
-$name = $_POST['name'];
-$pass = $_POST['Password'];
-echo ($name);
-echo ($pass)
-?>
+$db_host = "mydbinstance.czuwmj0nklpa.us-west-2.rds.amazonaws.com";
+$db_user = "keithy";
+$db_password = "hackathon";
+$db_name = "Persons";
+$mysqli = new mysqli($db_host,$db_user,$password,$db_name);
+$name = $_POST['email'];
+$pass = $_POST['pass'];
+@mysql_connect("$db_host","$db_user","$db_password") or die ("Could not connect to MySQL");
+@mysql_select_db("$db_name")
+echo "Successful Connection";
+mysqli_query($con, "INSERT INTO $db_name(Email, myPassword)
+		VALUES ('$email', '$pass')");
+		
+		$result = mysqli_query($mysqli,"SELECT * FROM $db_name");
+
+	while($row = mysqli_fetch_array($result)) {
+	  echo $row['Email'] . " " . $row['myPassword'];
+	  echo "<br>";
+	}	
+	?>
+</body>
+</html>
+	
+
+
+
 
