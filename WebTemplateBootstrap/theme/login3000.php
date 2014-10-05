3ab27547003f64225ed99a4ba47b60f204e31945
Karan <bNastyK@udel.edu>
2014-10-05 07:09:55 -0400

    new php

---
+++ WebTemplateBootstrap/theme/login3000.php
@@ -0,0 +1,24 @@
+<html>
+<body>
+<?php 
+// Check connection
+$con=mysqli_connect(
+	"mydbinstance.czuwmj0nklpa.us-west-2.rds.amazonaws.com",
+	"keithy",
+	"hackathon",
+	"innodb"
+	);
+	$email = $_POST["email"];
+	$pass = $_POST["pass"];
+	
+	mysqli_query($con, "INSERT INTO Persons(Email, myPassword)
+		VALUES ('$email', '$pass')");
+	$result = mysqli_query($con,"SELECT * FROM Users");
+
+	while($row = mysqli_fetch_array($result)) {
+	  echo $row['Email'] . " " . $row['myPassword'];
+	  echo "<br>";
+	}	
+	?>
+	</body>
+	</html>
