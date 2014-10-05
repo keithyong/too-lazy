69ec2aae77410650b31afa06229d1ba128e0ca9b
Karan <bNastyK@udel.edu>
2014-10-05 10:49:10 -0400

    added logincheck

--- WebTemplateBootstrap/theme/LoginCheck.php
+++ WebTemplateBootstrap/theme/LoginCheck.php
@@ -3,14 +3,13 @@ $con=mysqli_connect("mydbinstance.czuwmj0nklpa.us-west-2.rds.amazonaws.com","kei
 $email = $_POST["email"];
 $pass = $_POST["pass"];
 
-$result = SELECT myPassword FROM Users WHERE Email = $email;
+$result = "SELECT myPassword FROM Users WHERE Email = $email";
 	if ($pass == $result) {
 	header("Location:login3000.php");
 	echo "Valid Username";
 	}
 	else {
-	header("Location: index.html")
-	echo "Invalid Username and password);
+	header("Location: index.html");
+	echo "Invalid Username and password";
 	}
-	
-	
+?>
